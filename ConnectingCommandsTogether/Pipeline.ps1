23817980c476f3d726dfb92ad4d0ed4141c151c0
José Silva <josenilo.cp@gmail.com>
2023-10-08 17:30:26 -0300

    PSAvoidUsingCmdletAliases

--- ConnectingCommandsTogether/Pipeline.ps1
+++ ConnectingCommandsTogether/Pipeline.ps1
@@ -6,7 +6,7 @@ Write-Output ""
 
 Get-Process | where-object {$_.Handles -gt 1000}
 Write-Output ""
-Get-Process | where Handles -gt 1000
+Get-Process | Where-Object Handles -gt 1000
 Write-Output ""
 Write-Output "Handles Sorted"
 Write-Output ""
